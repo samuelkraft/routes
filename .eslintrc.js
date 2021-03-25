48fcd83ac5d605907270fcf04555edd962b63e17
Samuel Kraft <samuelkraft@me.com>
2021-03-25 06:57:44 +0100

    Eslint, lint-staged, prettier etc

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+    tsconfigRootDir: './',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier', 'import'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.tsx'],
+      },
+    ],
+    'react/jsx-one-expression-per-line': 0,
+    'react/require-default-props': 0,
+    'react/jsx-props-no-spreading': 0,
+    'jsx-a11y/anchor-is-valid': 0, // next-links require empty a tags
+    'jsx-a11y/label-has-associated-control': 0,
+    'import/prefer-default-export': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+  },
+  globals: {
+    JSX: true,
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+}
