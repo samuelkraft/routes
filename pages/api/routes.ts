fdd6684330f5b07aad6b64c9fbf58527a1a10943
Samuel Kraft <samuelkraft@me.com>
2022-09-18 22:03:06 +0100

    Add name to api

--- pages/api/routes.ts
+++ pages/api/routes.ts
@@ -8,6 +8,7 @@ export default (_: NextApiRequest, res: NextApiResponse) => {
   routes.forEach((route, i) => {
     routes[i].lat = route.geoJson.features[0].geometry.coordinates[0][1] // eslint-disable-line
     routes[i].lng = route.geoJson.features[0].geometry.coordinates[0][0] // eslint-disable-line
+    routes[i].name = route.geoJson.features[0].properties.name
   })
 
   res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate')
