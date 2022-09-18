a1507bd1c964139852d31357c80eb8373bdbdb88
Samuel Kraft <samuelkraft@me.com>
2022-09-18 15:50:01 +0100

    Add lat/lng to api route

--- pages/api/routes.ts
+++ pages/api/routes.ts
@@ -5,6 +5,11 @@ const gpxUtils = require('../../utils/gpxutils.js')
 export default (_: NextApiRequest, res: NextApiResponse) => {
   const routes = gpxUtils.routes.sort((a, b) => new Date(b.added).valueOf() - new Date(a.added).valueOf())
 
+  routes.forEach((route, i) => {
+    routes[i].lat = route.geoJson.features[0].geometry.coordinates[0][0] // eslint-disable-line
+    routes[i].lng = route.geoJson.features[0].geometry.coordinates[0][1] // eslint-disable-line
+  })
+
   res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate')
   res.status(200).json({ routes })
 }
