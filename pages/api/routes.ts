da389a023087f32efd2f367ecfb5cce12a962e6a
Samuel Kraft <samuelkraft@me.com>
2022-09-18 21:58:14 +0100

    Reverse lat/lng values

--- pages/api/routes.ts
+++ pages/api/routes.ts
@@ -6,8 +6,8 @@ export default (_: NextApiRequest, res: NextApiResponse) => {
   const routes = gpxUtils.routes.sort((a, b) => new Date(b.added).valueOf() - new Date(a.added).valueOf())
 
   routes.forEach((route, i) => {
-    routes[i].lat = route.geoJson.features[0].geometry.coordinates[0][0] // eslint-disable-line
-    routes[i].lng = route.geoJson.features[0].geometry.coordinates[0][1] // eslint-disable-line
+    routes[i].lat = route.geoJson.features[0].geometry.coordinates[0][1] // eslint-disable-line
+    routes[i].lng = route.geoJson.features[0].geometry.coordinates[0][0] // eslint-disable-line
   })
 
   res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate')
