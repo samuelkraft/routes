e055f308f6fef702e27b67446015121493fcff98
Samuel Kraft <samuelkraft@me.com>
2022-09-21 21:55:21 +0100

    Revert "Only return lat/lon"
    This reverts commit fb26eef4d7f6e284f78a58a834d60cc256ce0950.

--- pages/api/routes.ts
+++ pages/api/routes.ts
@@ -9,11 +9,6 @@ export default (_: NextApiRequest, res: NextApiResponse) => {
     routes[i].lat = route.geoJson.features[0].geometry.coordinates[0][1] // eslint-disable-line
     routes[i].lng = route.geoJson.features[0].geometry.coordinates[0][0] // eslint-disable-line
     routes[i].name = route.geoJson.features[0].properties.name
-    const coords = routes[i].geoJson.features[0].geometry.coordinates
-    // eslint-disable-next-line
-    coords.forEach((_, index) => {
-      coords[i] = [coords[index][0], coords[index][1]]
-    })
   })
 
   res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate')
