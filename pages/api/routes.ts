2b123dd666ed77a1ca365d4ddf729dad20dd3291
Samuel Kraft <samuelkraft@me.com>
2022-10-01 20:27:17 +0200

    Update routes.ts

--- pages/api/routes.ts
+++ pages/api/routes.ts
@@ -1,5 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+const simplify = require('simplify-geojson')
+const polyline = require('@mapbox/polyline')
+const gp = require('geojson-precision')
+
 const gpxUtils = require('../../utils/gpxutils.js')
 
 export default (_: NextApiRequest, res: NextApiResponse) => {
@@ -9,6 +13,13 @@ export default (_: NextApiRequest, res: NextApiResponse) => {
     routes[i].lat = route.geoJson.features[0].geometry.coordinates[0][1] // eslint-disable-line
     routes[i].lng = route.geoJson.features[0].geometry.coordinates[0][0] // eslint-disable-line
     routes[i].name = route.geoJson.features[0].properties.name
+
+    // Simplify geoJson and encode to polyline
+    const simplifiedGeoJson = simplify(gp(route.geoJson, 4), 0.0001)
+    routes[i].polyline = polyline.fromGeoJSON(simplifiedGeoJson.features[0].geometry)
+
+    // Remove unused geoJson
+    delete routes[i].geoJson
   })
 
   res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate')
