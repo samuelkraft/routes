3fc82fd40e8195b455d565be8ba47dfc71fecb0a
Samuel Kraft <samuelkraft@me.com>
2022-10-02 22:02:15 +0200

    Fix elevation array missing decimals

--- pages/api/routes.ts
+++ pages/api/routes.ts
@@ -10,23 +10,30 @@ export default (_: NextApiRequest, res: NextApiResponse) => {
   const routes = gpxUtils.routes.sort((a, b) => new Date(b.added).valueOf() - new Date(a.added).valueOf())
 
   routes.forEach((route, i) => {
-    routes[i].lat = route.geoJson.features[0].geometry.coordinates[0][1] // eslint-disable-line
-    routes[i].lng = route.geoJson.features[0].geometry.coordinates[0][0] // eslint-disable-line
-    routes[i].name = route.geoJson.features[0].properties.name
+    if (route.geoJson) {
+      routes[i].lat = route.geoJson.features[0].geometry.coordinates[0][1] // eslint-disable-line
+      routes[i].lng = route.geoJson.features[0].geometry.coordinates[0][0] // eslint-disable-line
+      routes[i].name = route.geoJson.features[0].properties.name
 
-    // Simplify geoJson and encode to polyline
-    const simplifiedGeoJson = simplify(gp(route.geoJson, 4), 0.0001)
-    routes[i].polyline = polyline.fromGeoJSON(simplifiedGeoJson.features[0].geometry)
+      // Simplify geoJson and encode to polyline
+      const simplifiedGeoJson = simplify(gp(route.geoJson, 4), 0.0001)
+      routes[i].polyline = polyline.fromGeoJSON(simplifiedGeoJson.features[0].geometry)
 
-    const elevations = simplifiedGeoJson.features[0].geometry.coordinates.map(coordinate => ({
-      value: coordinate[2],
-      distance: coordinate[3],
-    }))
+      const elevations = route.coordinates.map(coordinate => ({
+        value: coordinate[2],
+        distance: coordinate[3],
+      }))
 
-    routes[i].elevations = elevations
+      if (route.slug === 'salbohed') {
+        console.log(elevations)
+      }
 
-    // Remove unused geoJson
-    delete routes[i].geoJson
+      routes[i].elevations = elevations
+
+      // Remove unused geoJson
+      delete routes[i].geoJson
+      delete routes[i].coordinates
+    }
   })
 
   res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate')
